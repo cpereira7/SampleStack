cbaa18e8be07643d3674728b39709234cde1a498
cpereira7 <celso7ap@gmail.com>
2025-01-08 00:11:54 +0000

    Clean-up and refactoring of Program.cs

--- SampleStack.Repository/Program.cs
+++ SampleStack.Repository/Program.cs
@@ -1,66 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SampleStack.Repository;
-using SampleStack.Repository.Repositories;
-using SampleStack.Repository.Services;
-
-// See https://aka.ms/new-console-template for more information
 
 var host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration((context, config) => { config.AddEnvironmentVariables(); })
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.AddEnvironmentVariables();
+    })
     .ConfigureServices((context, services) =>
     {
+        services.AddApplicationServices(context.Configuration, context.HostingEnvironment);
 
-        if (context.HostingEnvironment.IsDevelopment())
-        {
-            services.AddSingleton(typeof(IRepository<>), typeof(MemoryRepository<>));
-        }
-        else
-        {
-            services.AddSingleton(typeof(IRepository<>), typeof(EfRepository<>));
-            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")!;
-
-            Console.WriteLine($"Using connection string: {connectionString}");
-
-            services.AddDbContext<DbContext, ApplicationDbContext>(options => options.UseNpgsql(connectionString));
-        }
-
-        services.AddSingleton<IPersonService, PersonService>();
+        services.AddScoped<ApplicationRunner>();
     })
     .Build();
 
 using var scope = host.Services.CreateScope();
 
-var hostingEnvironment = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
-if (!hostingEnvironment.IsDevelopment())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate();
+DatabaseInitializer.InitializeDataBase(host.Services);
 
-    Console.WriteLine("Database migrated");
-}
+var runner = scope.ServiceProvider.GetRequiredService<ApplicationRunner>();
 
-var personService = scope.ServiceProvider.GetRequiredService<IPersonService>();
-
-PrintPersonList(personService);
-
-personService.UpdatePerson(1, "John", "Smith");
-
-PrintPersonList(personService);
-
-personService.RemovePerson(1);
-
-PrintPersonList(personService);
-
-static void PrintPersonList(IPersonService personService)
-{
-    Console.WriteLine("-------");
-
-    foreach (var person in personService.GetPersons())
-    {
-        Console.WriteLine(person);
-    }
-}
-
+runner.Run();
---
+++ SampleStack.Repository/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SampleStack.Repository.Repositories;
+using SampleStack.Repository.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleStack.Repository
+{
+    internal static class ServiceCollectionExtensions
+    {
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            var isDevelopment = environment.IsDevelopment();
+
+            if (isDevelopment)
+            {
+                services.AddSingleton(typeof(IRepository<>), typeof(MemoryRepository<>));
+            }
+            else
+            {
+                services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+                var connectionString = configuration.GetConnectionString("DefaultConnection") ??
+                    throw new InvalidOperationException("Connection string not found");
+
+                Console.WriteLine($"Connection string: {connectionString}");
+
+                services.AddDbContext<DbContext, ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+            }
+
+            services.AddSingleton<IPersonService, PersonService>();
+        }
+    }
+}
