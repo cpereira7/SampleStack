c2279a03df753df82c50b6e82d5f53870144bfac
cpereira7 <celso7ap@gmail.com>
2025-01-16 21:09:03 +0000

    Added models and dtos

---
+++ SampleStack.AutoMapper/DTOs/OrderDto.cs
@@ -0,0 +1,34 @@
+﻿namespace SampleStack.AutoMapper.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+    }
+
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CustomerId { get; set; }
+        public List<OrderItemDto> Items { get; set; } = [];
+    }
+
+    public class OrderItemDto
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+    }
+}
---
+++ SampleStack.AutoMapper/Models/OrderModel.cs
@@ -0,0 +1,39 @@
+﻿namespace SampleStack.AutoMapper.Models
+{
+    internal class BaseModel
+    {
+        public int Id { get; set; }
+    }
+
+    internal class Customer : BaseModel
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+
+    internal class Order : BaseModel
+    {
+        public DateTime OrderDate { get; set; }
+        public required Customer Customer { get; set; }
+        public required List<OrderItem> Items { get; set; } = [];
+        public decimal TotalAmount => Items.Sum(item => item.TotalAmount);
+    }
+
+    internal class OrderItem : BaseModel
+    {
+        public int OrderId { get; set; }
+        public required Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public decimal TotalAmount => Product.Price * Quantity;
+    }
+
+    internal class Product : BaseModel
+    {
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+    }
+}
