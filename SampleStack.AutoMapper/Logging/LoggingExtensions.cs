4bd7e42a96d4b86e652ec20a6fcfa9e463f17eb9
cpereira7 <celso7ap@gmail.com>
2025-01-19 21:49:54 +0000

    Create LoggingExtensions.cs

---
+++ SampleStack.AutoMapper/Logging/LoggingExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SampleStack.AutoMapper.Logging
+{
+    public static partial class LoggingExtensions
+    {
+        [LoggerMessage(LogLevel.Debug, "Mapping started from {SourceType} to {DestinationType}")]
+        public static partial void DebugMappingStarted(this ILogger logger, Type sourceType, Type destinationType);
+
+        [LoggerMessage(LogLevel.Debug, "Mapping completed from {SourceType} to {DestinationType}")]
+        public static partial void DebugMappingCompleted(this ILogger logger, Type sourceType, Type destinationType);
+
+        [LoggerMessage(LogLevel.Error, "Error occurred while mapping from {SourceType} to {DestinationType}.")]
+        public static partial void ErrorMapping(this ILogger logger, Exception ex, Type sourceType, Type destinationType);
+    }
+}
