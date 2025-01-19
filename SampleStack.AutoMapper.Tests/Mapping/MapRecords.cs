110d96cbed6b7d939382ed7631671076f65f90f2
cpereira7 <celso7ap@gmail.com>
2025-01-19 21:52:15 +0000

    Added the unit test project for AutoMapper.

---
+++ SampleStack.AutoMapper.Tests/Mapping/MapRecords.cs
@@ -0,0 +1,14 @@
+﻿namespace SampleStack.AutoMapper.Mapping.Tests
+{
+    internal record SourceRecord
+    {
+        public string? Property1 { get; set; }
+        public int Property2 { get; set; }
+    }
+
+    internal record DestinationRecord
+    {
+        public string? Property1 { get; set; }
+        public int Property2 { get; set; }
+    }
+}
