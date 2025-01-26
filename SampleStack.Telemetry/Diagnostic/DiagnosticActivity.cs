39c1146c1a27ed6e3aaa68bd22cdc5ef4713db7f
cpereira7 <celso7ap@gmail.com>
2025-01-26 00:12:21 +0000

    Created a Diagnostic Activity Source

---
+++ SampleStack.Telemetry/Diagnostic/DiagnosticActivity.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace SampleStack.Telemetry.Telemetry
+{
+    public static class DiagnosticActivity
+    {
+        public static ActivitySource ActivitySource => new(DiagnosticNames.ServiceName, DiagnosticNames.ServiceVersion);
+
+        public static Activity? StartActivity(string activityName, object? parameter = null)
+        {
+            if (string.IsNullOrEmpty(activityName))
+                return null;
+
+            var activity = ActivitySource.CreateActivity(activityName, ActivityKind.Internal);
+
+            if (activity != null)
+            {
+                activity.Start();
+
+                if (parameter != null)
+                {
+                    activity.AddTag("parameter", parameter.ToString());
+                }
+            }
+
+            return activity;
+        }
+    }
+}
---
+++ SampleStack.Telemetry/Diagnostic/DiagnosticNames.cs
@@ -0,0 +1,17 @@
+﻿namespace SampleStack.Telemetry.Telemetry
+{
+    internal static class DiagnosticNames
+    {
+        public static string ServiceName => "samplestack";
+        public static string ServiceVersion => "1.0.0";
+
+        public static Dictionary<string, object> Attributes => new()
+        {
+           ["host.name"] = Environment.MachineName,
+           ["host.environment"] = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Development",
+           ["os.description"] = Environment.OSVersion.VersionString,
+           ["service.name"] = ServiceName,
+           ["service.version"] = ServiceVersion
+        };
+    }
+}
