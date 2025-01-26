adb5b7fcee53ed90b8a0a49b5c0af932f060e1df
cpereira7 <celso7ap@gmail.com>
2025-01-26 00:16:38 +0000

    Create TracesConfiguration.cs

---
+++ SampleStack.Telemetry/Telemetry/TracesConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace SampleStack.Telemetry.Telemetry
+{
+    internal static class TracesConfiguration
+    {
+        internal static void ConfigureOpenTelemetryTraces(this IServiceCollection services, IConfiguration configuration)
+        {
+            var resourceBuilder = ResourceBuilder.CreateDefault()
+                .AddService(DiagnosticNames.ServiceName, serviceVersion: DiagnosticNames.ServiceVersion)
+                .AddTelemetrySdk()
+                .AddAttributes(DiagnosticNames.Attributes);
+
+            services.AddOpenTelemetry().WithTracing(tracing => tracing
+                .SetResourceBuilder(resourceBuilder)
+                .AddSource(DiagnosticNames.ServiceName)
+                .AddHttpClientInstrumentation()
+                .AddOtlpExporter(otlpOptions =>
+                {
+                    otlpOptions.Endpoint = new Uri(configuration.GetConnectionString("OpenTelemetry"));
+                    otlpOptions.Protocol = OtlpExportProtocol.Grpc;
+                }));
+        }
+    }
+}
