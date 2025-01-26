73b9c08e3de844b85a5982664023c129462aee0e
cpereira7 <celso7ap@gmail.com>
2025-01-26 00:13:06 +0000

    Created the Activity Enricher classes

---
+++ SampleStack.Telemetry/Telemetry/ActivityEnricher.cs
@@ -0,0 +1,18 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using System.Diagnostics;
+
+namespace SampleStack.Telemetry.Telemetry
+{
+    internal class ActivityEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var activity = Activity.Current;
+
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("TraceId", activity.GetTraceId()));
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("SpanId", activity.GetSpanId()));
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("ParentSpanId", activity.GetParentSpanId()));
+        }
+    }
+}
---
+++ SampleStack.Telemetry/Telemetry/ActivityExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace SampleStack.Telemetry.Telemetry
+{
+    internal static class ActivityExtensions
+    {
+        public static string GetSpanId(this Activity activity)
+        {
+            return activity?.IdFormat switch
+            {
+                ActivityIdFormat.W3C => activity.SpanId.ToHexString(),
+                ActivityIdFormat.Hierarchical => activity.Id,
+                _ => null
+            } ?? string.Empty;
+        }
+
+        public static string GetTraceId(this Activity activity)
+        {
+            return activity?.IdFormat switch
+            {
+                ActivityIdFormat.W3C => activity.TraceId.ToHexString(),
+                ActivityIdFormat.Hierarchical => activity.RootId,
+                _ => null
+            } ?? string.Empty;
+        }
+
+        public static string GetParentSpanId(this Activity activity)
+        {
+            return activity?.IdFormat switch
+            {
+                ActivityIdFormat.W3C => activity.ParentSpanId.ToHexString(),
+                ActivityIdFormat.Hierarchical => activity.ParentId,
+                _ => null
+            } ?? string.Empty;
+        }
+    }
+}
